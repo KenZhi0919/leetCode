31dba083df5a0bc763c133f08c65065ac978b661
KenChen <ken.chen@thinkpower-info.com>
2021-01-22 17:08:07 +0800

    add array 26

---
+++ Algorithms/Array/26.ts
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let i = 0
+  while (i < nums.length) {
+    let j = i + 1
+    if (nums[i] === nums[j]) {
+      nums.splice(j, 1)
+    } else {
+      i++
+    }
+  }
+  return nums.length
+}
